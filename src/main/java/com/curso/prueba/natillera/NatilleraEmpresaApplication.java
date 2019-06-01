36c1e482c0ac715e3e7c37804b1963147179132b
Your Name <diegotobon1@gmail.com>
2019-06-01 15:45:22 -0500

    mi primer commit

---
+++ src/main/java/com/curso/prueba/natillera/NatilleraEmpresaApplication.java
@@ -0,0 +1,13 @@
+package com.curso.prueba.natillera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatilleraEmpresaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatilleraEmpresaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/prueba/natillera/NatilleraEmpresaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.prueba.natillera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatilleraEmpresaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
